5d4f76ce5fb457581ed24ad95acdae6e7a2659fb
rrdaniels85 <rachel.daniels@libertymutual.com>
2017-03-27 19:55:42 -0400

    Additional exercises
    straight-forward; no issues

--- javascripting/array-filtering.js
+++ javascripting/array-filtering.js
@@ -1,5 +1,5 @@
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 let filtered = numbers.filter(function evenNumbers(number){
-  return number % 2 === 0
+  return number jav% 2 === 0
 })
 console.log(filtered)
